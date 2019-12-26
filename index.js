98861706f3c8de4baeaabd840f6e0a0648f15bfb
genteanaranea <gentiaranea@gmail.com>
2019-12-26 14:35:08 +0000

    ほいすてぃんぐ

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(teddy)
+console.log(say1)
+console.log(say2())
+
+var teddy = 'bear'
+var say1 = function() {
+  console.log('aaaaaaaaaaaa')
+}
+
+function say2() {
+  console.log('bbbbbbbb')
+}
