e2c29f5d559a3f869eeea3c35807373b79848977
genteanaranea <gentiaranea@gmail.com>
2019-12-29 08:51:49 +0000

    スコープ

--- index.js
+++ index.js
@@ -1,12 +1,75 @@
-console.log(teddy)
-console.log(say1)
-console.log(say2())
+// console.log(teddy)
+// console.log(say1)
+// console.log(say2())
 
-var teddy = 'bear'
-var say1 = function() {
-  console.log('aaaaaaaaaaaa')
-}
+// var teddy = 'bear'
+// var say1 = function() {
+//   console.log('aaaaaaaaaaaa')
+// }
 
-function say2() {
-  console.log('bbbbbbbb')
-}
+// function say2() {
+//   console.log('bbbbbbbb')
+// }
+
+// one = undefined
+// var one = 1;
+// var one = 2;
+// console.log(one)
+
+
+// var dog = 'dog'
+// function hoisting() {
+//   console.log(dog)
+//   var dog = 'cat'
+//   console.log(dog)
+// }
+// hoisting();
+
+// kaiseki
+// var dog
+// function hoisting()
+// var dog
+
+// zikkou
+// var dog = 'dog'
+// hosisting()
+// var dog = undifined
+// console.log(dog) // undifined
+// var dog = 'cat'
+// console.log(dog) //cat
+
+// varで変数を宣言した時はglobal()に格納される
+// var dog = undefined
+// console.log(dog)
+// var dog = 'dog' 
+
+// console.log(sing())
+// function sing() {
+//   console.log('lalal')
+// }
+
+//即時関数
+// (function(){
+//   var j = 'j'
+//   console.log('j')
+// }())
+
+// function x(){
+//   var x1 = 'x1'
+//   console.log('x1')
+// }
+
+// var y = function(){
+//   var y1 = 'y1'
+//   console.log('y')
+// }
+
+//x() //hoistingが起きるはず
+//y() //hoistingは起きない
+//関数スコープなのでむり　console.log(j)
+//関数スコープなのでむり　console.log(x1)
+//関数スコープなのでむり　console.log(y)
+
+
+
+
