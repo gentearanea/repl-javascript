9667f0495ed7ee77271b8b7df7f1ccb788e549ee
genteanaranea <gentiaranea@gmail.com>
2019-12-30 08:25:16 +0000

    js nan

--- index.js
+++ index.js
@@ -70,6 +70,113 @@
 //関数スコープなのでむり　console.log(x1)
 //関数スコープなのでむり　console.log(y)
 
+// const wizard = {
+//   name: 'marlin',
+//   health: 50,
+//   heal(num1, num2) {
+//     return this.health += num1 + num2
+//   },
+// }
 
+// const archer = {
+//   name: 'sirou',
+//   health: 30,
+// }
 
+// console.log(wizard.name)
+// console.log(wizard.health)
+// console.log(archer.health)
+
+// const healArcher = wizard.heal.bind(archer, 100, 30)
+// healArcher()
+
+// var a = 'a'
+// var b = 'b'
+// var a = b
+// b = 'b'
+// console.log(a)
+// console.log(b)
+
+//replacerにfunctionを指定した場合
+// function replacer(key, value) {
+//     if (typeof value === 'string') {
+//         return undefined;
+//     }
+//     return value;
+// }
+
+// var obj = {
+//     a: 1,
+//     b: "Moji",
+//     c: [1, 2, 3, 4],
+//     d: {
+//         "A": 1,
+//         "B": 2
+//     }
+// };
+
+// console.log(JSON.stringify(obj, replacer, "\t"));
+
+//replacerに配列のみを指定した場合
+//var replacer = ['a','d','A'];
+
+// var obj = {
+//     a: 1,
+//     b: "Moji",
+//     c: [1, 2, 3, 4],
+//     d: {
+//         "A": 1,
+//         "B": 2
+//     }
+// };
+
+// console.log(JSON.stringify(obj, replacer, "\t"));
+
+// var obj = {
+//     a: 1,
+//     b: "Moji",
+//     c: [1, 2, 3, 4],
+//     d: {
+//         "A": 1,
+//         "B": 2
+//     }
+// };
+//console.log(JSON.stringify(obj));
+//console.log(JSON.stringify(obj,undefined,1));
+
+// リストの使い方を間違えている場合
+// const number = 100
+// const string = "Jay"
+// let obj1 = {
+//   value: "a"
+// }
+// let obj2 = {
+//   value: "b"
+// }
+// let obj3 = obj2;
+
+// function change(number, string, obj1, obj2) {
+//     number = number * 10;
+//     string = "Pete";
+//     obj1 = obj2; //obj1 = obj2
+//     obj2.value = "c"; //obj2.value = c
+// }
+ 
+// change(number, string, obj1, obj2);
+
+// // 参照渡しなのでobj2, obj3の値が変わってしまう
+// //Guess the outputs here before you run the code: 
+// console.log(number); 
+// console.log(string);
+// console.log(obj1.value);
+// console.log(obj2)
+// console.log(obj3)
+
+//isを使って真理値を判定する。
+// console.log(-0 === +0)
+// Object.is(-0, +0)
+
+// NaNの場合は===を使用する
+// console.log(NaN === NaN)
+// Object.is(NaN, NaN)
 
